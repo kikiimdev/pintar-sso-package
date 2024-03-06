f13ecc48dcb8d393a6873e45dc1bfff52b992c39
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-03-06 12:30:59 +0800

    feat: add option post_login and post_bind

--- src/config/pintar_sso.php
+++ src/config/pintar_sso.php
@@ -4,4 +4,6 @@ return [
     'client_id' => env('PINTAR_SSO_CLIENT_ID'),
     'client_secret' => env('PINTAR_SSO_CLIENT_SECRET'),
     'auth_domain' => env('PINTAR_SSO_AUTH_DOMAIN', 'https://sso.banjarmasinkota.go.id'),
+    'post_login' => env('PINTAR_SSO_POST_LOGIN'),
+    'post_bind' => env('PINTAR_SSO_POST_BIND'),
 ];
--- src/routes/sso.php
+++ src/routes/sso.php
@@ -26,7 +26,7 @@ Route::prefix('sso')->group(function () {
                 'pintar_id' => $pintar_account->id,
             ]);
 
-            return $user;
+            return response()->intended(config('post_bind'));
         } catch (\Exception $error) {
             return response()->json($error->getMessage(), 500);
         }
@@ -52,7 +52,7 @@ Route::prefix('sso')->group(function () {
 
             $sso->log_activity($request, 'LOGIN');
 
-            return redirect()->intended(RouteServiceProvider::HOME);
+            return response()->intended(config('post_login'));
         } catch (\Exception $error) {
             return response()->json($error->getMessage(), 500);
         }
