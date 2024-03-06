94eb1171183185fa98ddc68e2db7842392d72397
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-03-06 15:47:38 +0800

    refactor: PintarAccount trait

--- src/Traits/HasPintarAccount.php
+++ src/Traits/HasPintarAccount.php
@@ -2,13 +2,69 @@
 
 namespace Banjarmasinkota\PintarSSO\Traits;
 
+use Illuminate\Http\Request;
 use App\Models\PintarAccount;
+use Illuminate\Support\Facades\Auth;
+use Banjarmasinkota\PintarSSO\PintarSSO;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 trait HasPintarAccount
 {
-    public function pintar_account(): HasOne
-    {
-       return $this->hasOne(PintarAccount::class);
+  public function pintar_account(): HasOne
+  {
+    return $this->hasOne(PintarAccount::class);
+  }
+
+  public static function redirect_to_authorization_url()
+  {
+    $callback_url = url('sso/bind/callback');
+    $sso = new PintarSSO($callback_url);
+    return $sso->redirect_to_authorization_url();
+  }
+
+  public static function get_user_from_callback(Request $request, bool $redirect = false)
+  {
+    $sso = new PintarSSO();
+    $pintar_account = $sso->get_user_from_callback($request);
+
+    $account = request()->user()->pintar_account()->updateOrCreate([
+      'pintar_id' => $pintar_account->id,
+    ], [
+      'pintar_id' => $pintar_account->id,
+    ]);
+
+    if ($redirect) {
+      return response()->redirectToIntended(url(config('pintar_sso.post_bind')));
     }
+
+    return $account;
+  }
+
+  public static function redirect_to_login_page()
+  {
+    $callback_url = url('sso/login/callback');
+    $sso = new PintarSSO($callback_url);
+
+    return $sso->redirect_to_authorization_url();
+  }
+
+  public static function login_from_callback(Request $request, bool $redirect = false)
+  {
+    $sso = new PintarSSO();
+    $pintar_account = $sso->get_user_from_callback($request);
+
+    // find the user that has $pintar_sso_user->id
+    $user = static::whereRelation('pintar_account', 'pintar_id', $pintar_account->id)->first();
+
+    // Login using user id
+    Auth::loginUsingId($user->id);
+
+    $sso->log_activity($request, 'LOGIN');
+
+    if ($redirect) {
+      return response()->redirectToIntended(url(config('pintar_sso.post_login')));
+    }
+
+    return $user;
+  }
 }
--- src/routes/sso.php
+++ src/routes/sso.php
@@ -1,60 +1,31 @@
 <?php
 
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Banjarmasinkota\PintarSSO\PintarSSO;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('sso')->group(function () {
-    Route::get('/bind', function (Request $request) {
-        $callback_url = url('sso/bind/callback');
-        $sso = new PintarSSO($callback_url);
+  Route::get('/bind', function (Request $request) {
+    return User::redirect_to_authorization_url();
+  });
 
-        return $sso->redirect_to_authorization_url();
-    });
+  Route::get('/bind/callback', function (Request $request) {
+    try {
+      return User::get_user_from_callback($request, true);
+    } catch (\Exception $error) {
+      return response()->json($error->getMessage(), 500);
+    }
+  });
 
-    Route::get('/bind/callback', function (Request $request) {
-        try {
-            $sso = new PintarSSO();
+  Route::get('/login', function (Request $request) {
+    return User::redirect_to_login_page();
+  });
 
-            $pintar_account = $sso->get_user_from_callback($request);
-            $user = request()->user()->pintar_account()->updateOrCreate([
-                'pintar_id' => $pintar_account->id,
-            ], [
-                'pintar_id' => $pintar_account->id,
-            ]);
-
-            return response()->redirectToIntended(url(config('pintar_sso.post_bind')));
-        } catch (\Exception $error) {
-            return response()->json($error->getMessage(), 500);
-        }
-    });
-
-    Route::get('/login', function (Request $request) {
-        $callback_url = url('sso/login/callback');
-        $sso = new PintarSSO($callback_url);
-
-        return $sso->redirect_to_authorization_url();
-    });
-
-    Route::get('/login/callback', function (Request $request) {
-        try {
-            $sso = new PintarSSO();
-            $pintar_account = $sso->get_user_from_callback($request);
-
-            // find the user that has $pintar_sso_user->id
-            $user = User::whereRelation('pintar_account', 'pintar_id', $pintar_account->id)->first();
-
-            // Login using user id
-            Auth::loginUsingId($user->id);
-
-            $sso->log_activity($request, 'LOGIN');
-
-            return response()->redirectToIntended(url(config('pintar_sso.post_login')));
-        } catch (\Exception $error) {
-            return response()->json($error->getMessage(), 500);
-        }
-    });
+  Route::get('/login/callback', function (Request $request) {
+    try {
+      return User::login_from_callback($request, true);
+    } catch (\Exception $error) {
+      return response()->json($error->getMessage(), 500);
+    }
+  });
 });
