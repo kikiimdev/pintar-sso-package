47e53c3c32da9a9fe59a609ef69a7f6002c5d36c
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-06-10 05:13:40 +0800

    fix: only show one-tap if not authenticated

--- src/components/pintar-sso-one-tap.blade.php
+++ src/components/pintar-sso-one-tap.blade.php
@@ -5,24 +5,26 @@
 ])
 
 <div>
-    <iframe id="sso-one-tap" src="{{config('pintar_sso.auth_domain')}}/oauth/one-tap?client_id={{config('pintar_sso.client_id')}}&redirect_uri={{config('pintar_sso.post_login')}}" style="z-index: 999; position: fixed; right: 0px; top: 0px; width: 500px; max-width: 100vw; height: 50vh"></iframe>
-    <script>
-        window.addEventListener('message', function(event) {
-            const isWappalyzerPlugin = !!event.data?.wappalyzer
-            if (!isWappalyzerPlugin) {
-                // console.log("Message received from the child: ", event.data); // Message received from child
-            }
-            const isCloseCommand = event.data == "sso-one-tap-close"
-            if (isCloseCommand) {
-                const element = document.getElementById("sso-one-tap");
-                element.remove();
-            }
+    @unless (Auth::check())
+        <iframe id="sso-one-tap" src="{{config('pintar_sso.auth_domain')}}/oauth/one-tap?client_id={{config('pintar_sso.client_id')}}&redirect_uri={{config('pintar_sso.post_login')}}" style="z-index: 999; position: fixed; right: 0px; top: 0px; width: 500px; max-width: 100vw; height: 50vh"></iframe>
+        <script>
+            window.addEventListener('message', function(event) {
+                const isWappalyzerPlugin = !!event.data?.wappalyzer
+                if (!isWappalyzerPlugin) {
+                    // console.log("Message received from the child: ", event.data); // Message received from child
+                }
+                const isCloseCommand = event.data == "sso-one-tap-close"
+                if (isCloseCommand) {
+                    const element = document.getElementById("sso-one-tap");
+                    element.remove();
+                }
 
-            const isConfirmCommand = event.data == "sso-one-tap-confirm"
-            if (isConfirmCommand) {
-                // console.log("Confirmed: ", event.data);
-                window.location.href = `{{config('app.url')}}/sso/login?authorized`
-            }
-        });
-    </script>
+                const isConfirmCommand = event.data == "sso-one-tap-confirm"
+                if (isConfirmCommand) {
+                    // console.log("Confirmed: ", event.data);
+                    window.location.href = `{{config('app.url')}}/sso/login?authorized`
+                }
+            });
+        </script>
+    @endunless
 </div>
